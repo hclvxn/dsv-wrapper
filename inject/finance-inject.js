713ee65aee6cc28ffddeb80d90d4c072677d1200
Logan Barnett <logustus@gmail.com>
2016-05-13 16:23:07 -0700

    add other shipment data to display and add column wrap

--- inject/finance-inject.js
+++ inject/finance-inject.js
@@ -15,11 +15,29 @@ var followMouse = function(e) {
 
 var createTooltip = function() {
   tooltipContainer = document.createElement('div');
+  // technically we're not supposed to wrap dd and dt tags in divs, but
+  // it screws up the column wrapping by wrapping between a dt and its dd
   tooltipContainer.innerHTML = '<dl class="tooltip">' +
+    '<div>' +
     '<dt>Shipment Number</dt>' +
     '<dd data-id="shipment-number">2341256</dd>' +
+    '</div>' +
+    '<div>' +
+    '<dt>Origin</dt>' +
+    '<dd data-id="origin"></dd>' +
+    '</div>' +
+    '<div>' +
+    '<dt>Destination</dt>' +
+    '<dd data-id="destination"></dd>' +
+    '</div>' +
+    '<div>' +
     '<dt>Mode</dt>' +
-    '<dd data-id="mode">Air</dd>' +
+    '<dd data-id="mode"></dd>' +
+    '</div>' +
+    '<div>' +
+    '<dt>Status</dt>' +
+    '<dd data-id="status"></dd>' +
+    '</div>' +
   '</dl>';
   tooltipContainer.classList.add('tooltip-container');
   document.body.appendChild(tooltipContainer);
@@ -40,8 +58,13 @@ var showQuickShipmentInfo = function(event) {
     var result = JSON.parse(xhr.responseText);
     tooltip.classList.remove('remove');
     setTimeout(function() { tooltip.classList.add('show'); }, 1);
-    setChildText(tooltip, 'shipment-number', result.shipments[0].shipmentNbr);
-    setChildText(tooltip, 'mode', result.shipments[0].productTypeDescription);
+    var shipment = result.shipments[0];
+    console.log(shipment);
+    setChildText(tooltip, 'shipment-number', shipment.shipmentNbr);
+    setChildText(tooltip, 'mode', shipment.productTypeDescription);
+    setChildText(tooltip, 'origin', shipment.originTariffPt);
+    setChildText(tooltip, 'destination', shipment.destinationTariffPt);
+    setChildText(tooltip, 'status', shipment.statusHistory[0].categoryText);
   });
   // xhr.open('GET', 'https://sinupq10.go2uti.com:8443/api/v1/shipment/' +
   // xhr.withCredentials = true;
